8b6feda8c33d687ec7484ff495542ecf1d3e44a3
My NameNastya <kath.rine.boom1@gmail.com>
2020-11-04 17:44:23 +0200

    task4

---
+++ Second_lesson/Operations_with_variables/Task_4/script.js
@@ -0,0 +1,6 @@
+var a = prompt('Введите первое число', '');
+var b = prompt('Введите второе число', '');
+var c = prompt('Введите третье число', '');
+
+var res = a < b && b < c ;
+console.log(res);
