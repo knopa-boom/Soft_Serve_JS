74c1752fbfce37d3b3e7f14f4ff7a3b6c1a16ad4
Nastya <42748055+knopa-boom@users.noreply.github.com>
2020-11-05 18:44:42 +0200

    Update script.js

--- Third_lesson/Cycles/Task_2/script.js
+++ Third_lesson/Cycles/Task_2/script.js
@@ -1,9 +1,8 @@
- var n = 13;
- 
+ var n = 123456;
+
  while (n > 0) {
-	 console.log(n % 10);
-	 console.log(parseInt(n/10));
-	 break;
+     console.log(n % 10);
+     n = parseInt(n / 10);
+
+
  }
- 
- 
