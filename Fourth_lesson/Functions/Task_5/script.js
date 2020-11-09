3001189f97af2418d62b5a407bfb545622266282
Nastya <42748055+knopa-boom@users.noreply.github.com>
2020-11-09 14:22:47 +0200

    Update script.js

--- Fourth_lesson/Functions/Task_5/script.js
+++ Fourth_lesson/Functions/Task_5/script.js
@@ -1,6 +1,12 @@
 function digitN(k, n) {
+    for (var i = 0; k > 1; i++) {
+        k /= 10; // найдем кол-во цифр в k
+
+    }
+
+
 
 }
 
-var res = digitN(47, 5);
+var res = digitN(1845, 3); // ждем ответ 8
 console.log(res);
