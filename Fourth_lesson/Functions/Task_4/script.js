d7f7648c92983172c988e8acdd1ef8e6da6c1bd7
Nastya <42748055+knopa-boom@users.noreply.github.com>
2020-11-09 13:50:06 +0200

    Update script.js

--- Fourth_lesson/Functions/Task_4/script.js
+++ Fourth_lesson/Functions/Task_4/script.js
@@ -1,12 +1,35 @@
+//1-й вариант 
+
+// function calc(a, b, op) {
+//     if (op == 1) {
+//         return a - b;
+//     } else if (op == 2) {
+//         return a * b;
+//     } else if (op == 3) {
+//         return a / b;
+//     } else {
+//         return a + b;
+//     }
+// }
+
+// var res = calc(2, 3, 2);
+// console.log(res);
+
+//2-й вариант 
 function calc(a, b, op) {
-    if (op == 1) {
-        return a - b;
-    } else if (op == 2) {
-        return a * b;
-    } else if (op == 3) {
-        return a / b;
-    } else {
-        return a + b;
+    switch (op) {
+        case 1:
+            return a - b;
+            break;
+        case 2:
+            return a * b;
+            break;
+        case 3:
+            return a / b;
+            break;
+        default:
+            return a + b;
+            break;
     }
 }
 
