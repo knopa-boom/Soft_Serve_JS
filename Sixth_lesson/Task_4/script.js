77f433bb20b295547625ffa42ee5bb3a0b6a1a7b
Nastya <42748055+knopa-boom@users.noreply.github.com>
2020-11-16 17:05:32 +0200

    Update script.js

--- Sixth_lesson/Task_4/script.js
+++ Sixth_lesson/Task_4/script.js
@@ -4,8 +4,20 @@ var MP3 = {
     connectors: ['mini-usb', 'mini-jack'],
     weight: 38,
     format: ['mp3', 'wav', 'wma'],
+    state: 'off',
+    play: function() {
+        MP3.state = 'on';
+    },
+    pause: function() {
+        MP3.state = 'pause';
+    }
 }
 console.log(MP3.model);
 console.log(MP3.material);
 console.log(MP3.format[1]);
 console.log(MP3.connectors[0]);
+
+MP3.play();
+console.log(MP3.state);
+MP3.pause();
+console.log(MP3.state);
