f5465a992ca453aca051eb6c9f27256eef705bf7
Nastya <42748055+knopa-boom@users.noreply.github.com>
2020-11-16 16:57:40 +0200

    Update script.js

--- Sixth_lesson/Task_3/script.js
+++ Sixth_lesson/Task_3/script.js
@@ -2,15 +2,20 @@ var tv = {
     currentChannel: 1,
     nextChannel: function() {
         tv.currentChannel += 1;
-        console.log(tv.currentChannel);
+    },
+    previousChannel: function() {
+        tv.currentChannel = tv.currentChannel - 1;
     },
     setChannel: function(number) {
         tv.currentChannel = number;
-        console.log(tv.currentChannel);
-
     }
-
 };
 
 tv.nextChannel();
+console.log(tv.currentChannel);
+
+tv.previousChannel();
+console.log(tv.currentChannel)
+
 tv.setChannel(3);
+console.log(tv.currentChannel);
