0c4c5994a83a696a1f4420e0f56ed6d5b8eeb265
Nastya <42748055+knopa-boom@users.noreply.github.com>
2020-11-19 16:24:57 +0200

    Update script.js

--- Sixth_lesson/Task_5/script.js
+++ Sixth_lesson/Task_5/script.js
@@ -26,3 +26,31 @@ var obj = {
 console.log(obj.users[0].name);
 console.log(obj.users[1].languages[0]);
 console.log(obj.users[2].experience);
+
+
+var users = {
+    user1: {
+        name: 'Peter',
+        age: 28,
+        experience: 1,
+        languages: ['Java', 'JavaScript', 'SQL'],
+    },
+
+    user2: {
+        name: 'Ivam',
+        age: 30,
+        experience: 5,
+        languages: ['HTML', 'CSS', 'JavaScript'],
+    },
+
+    user3: {
+        name: 'Jake',
+        age: 32,
+        experience: 4,
+        languages: ['Python', 'SQL'],
+    },
+}
+
+
+console.log(users.user1.name);
+console.log(users.user1.languages[1]);
