13c255c4ac04f6b5ea3ecc460499bcca4f7af4df
Nastya <42748055+knopa-boom@users.noreply.github.com>
2020-12-07 12:09:36 +0200

    Update script.js

--- Twelvth_lesson/Array/Task_4/script.js
+++ Twelvth_lesson/Array/Task_4/script.js
@@ -1,11 +1,3 @@
-// var arr = ["Tom", "Sam", "Bob"];
-// var arrLength = [];
-// for (var i = 0; i < arr.length; i++) {
-//     arrLength[i] = "<li>" + arr[i] + "</li>";
-// }
-// console.log(arrLength); // ["<li>Tom</li>", "<li>Sam</li>", "<li>Bob</li>"]
-
-
 var arr = ["Tom", "Sam", "Bob"];
 
 function li(elem) {
