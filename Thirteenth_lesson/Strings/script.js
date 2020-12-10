e02ef313035a604603334dbe0a987b9efc612e8f
Nastya <42748055+knopa-boom@users.noreply.github.com>
2020-12-10 17:43:14 +0200

    Add files via upload

---
+++ Thirteenth_lesson/Strings/script.js
@@ -0,0 +1,6 @@
+let names = {
+    first: "Tom",
+    second: "Sam",
+    third: "Ray"
+};
+console.log(`Первый: -${names.first}- , Второй: -"${names.second}",  Третий: => '${names.third}'`); // "Первый: -Tom-, Второй - "Sam", Третий => 'Ray'"
