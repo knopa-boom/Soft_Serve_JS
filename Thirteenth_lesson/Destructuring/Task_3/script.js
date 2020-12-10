c3bde425cfa5dbdc55a2c19cd256ef6cbbcbecad
Nastya <42748055+knopa-boom@users.noreply.github.com>
2020-12-10 16:26:41 +0200

    Update script.js

--- Thirteenth_lesson/Destructuring/Task_3/script.js
+++ Thirteenth_lesson/Destructuring/Task_3/script.js
@@ -5,7 +5,7 @@ let data = {
     ages: [20, 24, 22, 26],
 };
 
-let { names: [, name2], ages: [, age2], names: [, , , name4], ages: [, , , age4] } = data;
+let { names: [, name2, , name4], ages: [, age2, , age4] } = data;
 console.log(name2); // "Tom"
 console.log(age2); // 24
 console.log(name4); // "Bob"
